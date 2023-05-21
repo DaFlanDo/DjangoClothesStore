98d677f3b59b4970cb394306ebfc1e2e60fd5422
DaFlanDo <123935478+DaFlanDo@users.noreply.github.com>
2023-05-21 20:13:32 +0300

    Tets

---
+++ products/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from products.models import ProductCategory,Product
+# Create your views here.
+def index(request):
+    return render(request, 'products/index.html')
+def products(request):
+    context ={
+    'products': Product.objects.all(),
+    'categories': ProductCategory.objects.all()
+
+    }
+    return render(request, 'products/products.html',context=context)
