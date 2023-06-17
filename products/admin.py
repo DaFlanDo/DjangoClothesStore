29bcec934d468bb27886f010dee2217b810f0256
DaFlanDo <123935478+DaFlanDo@users.noreply.github.com>
2023-06-17 15:54:19 +0300

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from products.models import ProductCategory, Product
+from products.models import ProductCategory, Product, Baskets
+
 # Register your models here.
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'quantity','created_at')
+    fields = ('name', 'category', 'price', 'quantity','image')
+
+
+class BasketsAdmin(admin.TabularInline):
+    model = Baskets
+    fields = ('user', 'product','quantity','created_time' )
+    readonly_fields = ('created_time',)
+    extra = 0
+
